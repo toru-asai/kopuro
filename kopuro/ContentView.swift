d98ff968e05aa870ee7323b325bc10e11f4860ce
toru-asai <shiomikkomasa@icloud.com>
2020-01-19 22:29:53 +0900

    Initial Commit

---
+++ kopuro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kopuro
+//
+//  Created by 坂本雅大 on 2020/01/19.
+//  Copyright © 2020 坂本雅大. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
